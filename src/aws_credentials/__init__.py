32071c8efd9fafe7e57d2b3c5f33fde080f7a692
James Kabbes <james.kabbes@gmail.com>
2023-01-19 17:09:20 -0600

    adding default role

--- src/aws_credentials/__init__.py
+++ src/aws_credentials/__init__.py
@@ -7,3 +7,10 @@ from .AWS_Creds import AWS_Creds
 from .Client import Client
 
 client = Client()
+
+Cred = None
+cred_dict = {}
+
+if client.cfg['role'] != None:
+    Cred = client.Creds[ client.cfg['role'] ]
+    cred_dict = Cred.dict
